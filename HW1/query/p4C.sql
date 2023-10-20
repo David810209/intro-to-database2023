394f0fdfc93c79f0e7a4441e9867c653d4467b75
Zichen55 <90104520+David810209@users.noreply.github.com>
2023-10-21 04:42:25 +0800

    Create p4C.sql

---
+++ HW1/query/p4C.sql
@@ -0,0 +1,11 @@
+WITH themes_set(name, total_set) AS(
+        SELECT t.name , COUNT(s.name)
+        FROM sets s, themes t
+        WHERE t.id = s.theme_id
+        GROUP BY t.name)
+SELECT name, total_set as max_set
+FROM themes_set
+WHERE total_set = (
+        SELECT MAX(total_set)
+        FROM themes_set
+    );
